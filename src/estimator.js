4d42f7000c3d220a46f01f509374a92dd9ca07a5
Alexander <zanderthuo@gmail.com>
2020-04-13 10:35:53 +0300

    Fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,61 +1,61 @@
 const covid19ImpactEstimator = (data) => {
-    // input in data
-  
-    // Destructuring data
-  const {
+  //input in data
+
+  //Destructuring data
+  const{
     timeToElapse,
     reportedCases,
     totalHospitalBeds
   } = data;
-  
 
-    //    calculate InfectionsByRequestedTime
+  //calculate infectionsByRequestedTime
   const calculateInfectionsByRequestedTime = (currentlyInfected) => {
-      // eslint-disable-next-line radix
-   const factor = parseInt(timeToElapse / 3);
-   return currentlyInfected * (2 ** factor);
-  };
-  
-  // calculate AvailableBeds
-  const calculateAvailableBeds = (severeCasesByRequestedTime) => {
-   const bedsAvailable = totalHospitalBeds * 0.35;
-   const shortage = bedsAvailable - severeCasesByRequestedTime;
-   const result = shortage < 0 ? shortage : bedsAvailable;
-    return parseInt(result);
+    const factor = parseInt(timeToElapse / 3);
+    return currentlyInfected * (2 ** factor);      
   };
 
-    // best case estimation
+  //calculate AvailableBeds
+  const calculateAvailableBeds = (severeCasesByRequestedTime) => {
+    const bedsAvailable = totalHospitalBeds * 0.35;
+    const shortage = bedsAvailable - severeCasesByRequestedTime;
+    const result = shortage < 0 ? shortage : bedsAvailable;
+     return parseInt(result);    
+  };
+
+  //best case estimation
   const impact = {};
-  
-    // challenge 1
+
+  //challenge 1
   impact.currentlyInfected = reportedCases * 10;
   impact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(impact.currentlyInfected);
   //challenge 2
-  impact.servereCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
   impact.hospitalBedsByRequestedTime = calculateAvailableBeds(impact.severeCasesByRequestedTime);
   //challenge 3
   impact.casesForICUByRequestedTime = impact.infectionsByRequestedTime * 0.05;
   impact.casesFprVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02;
   impact.dollarsInFlight = impact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse;
-  
-    // the severe case estimation
+
+  //the severe case estimation
   const severeImpact = {};
-    // challenge 1
+
+  //challenge 1
   severeImpact.currentlyInfected = reportedCases * 50;
   severeImpact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(severeImpact.currentlyInfected);
-  // challenge 2
+  //challenge 2
   severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
   severeImpact.hospitalBedsByRequestedTime = calculateAvailableBeds(severeImpact.severeCasesByRequestedTime);
- // challenge 3
- severeImpact.casesForICUByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
- severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
- severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse;
+  //challenge 3
+  severeImpact.casesForICUByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
+  severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
+  severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse;
+  
   return {
-    data,
-    impact,
-    severeImpact
+   data,
+   impact,
+   severeImpact
   };
 
-};
+}
 
-export default covid19ImpactEstimator;  
+export default covid19ImpactEstimator;
