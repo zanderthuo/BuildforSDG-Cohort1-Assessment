1970b3f0435e75fecd68d783250752bb31f03fe1
Alexander <zanderthuo@gmail.com>
2020-04-13 09:02:44 +0300

    fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,40 +1,41 @@
 /* eslint-disable max-len */
 /* eslint-disable indent */
+
 const covid19ImpactEstimator = (data) => {
     // input in data
   
     // Destructuring data
-    const {
-      timeToElapse,
-      reportedCases
-    } = data;
+  const {
+    timeToElapse,
+    reportedCases
+  } = data;
   
 
     //    calculate InfectionsByRequestedTime
-    const calculateInfectionsByRequestedTime = (currentlyInfected) => {
+  const calculateInfectionsByRequestedTime = (currentlyInfected) => {
       // eslint-disable-next-line radix
-      const factor = parseInt(timeToElapse / 3);
-      return currentlyInfected * (2 ** factor);
-    };
+    const factor = parseInt(timeToElapse / 3);
+    return currentlyInfected * (2 ** factor);
+  };
   
     // best case estimation
-    const impact = {};
+  const impact = {};
   
     // challenge 1
-    impact.currentlyInfected = reportedCases * 10;
-    impact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(impact.currentlyInfected);
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(impact.currentlyInfected);
   
   
     // the severe case estimation
-    const severeImpact = {};
+  const severeImpact = {};
     // challenge 1
-    severeImpact.currentlyInfected = reportedCases * 50;
-    severeImpact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(severeImpact.currentlyInfected);
+  severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(severeImpact.currentlyInfected);
   
-    return {
-      data,
-      impact,
-      severeImpact
-    };
+  return {
+    data,
+    impact,
+    severeImpact
   };
+};
 export default covid19ImpactEstimator;  
