ddddc0c4c097420e3bc59efdb581288d95ca0537
Alexander <zanderthuo@gmail.com>
2020-04-13 12:21:36 +0300

    fix

--- src/estimator.js
+++ src/estimator.js
@@ -18,13 +18,13 @@ const covid19ImpactEstimator = (data) => {
   // normalize days; check for weeks and months if used
   switch (periodType.trim().toLowerCase()) {
     case 'months':
-      (timeToElapse * 30) / 3;
+      timeToElapse = (timeToElapse * 30) / 3;
       break;
     case 'weeks':
-      (timeToElapse * 7) / 3;
+      timeToElapse = (timeToElapse * 7) / 3;
       break;
     case 'days':
-      timeToElapse / 3;
+      timeToElapse = timeToElapse / 3;
       break;
     default:
   }
