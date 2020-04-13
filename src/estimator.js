08de8b8aa6fdeb8c267ed333b58c3cc75e0c7c7a
Alexander <zanderthuo@gmail.com>
2020-04-13 15:52:39 +0300

    fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,47 +1,63 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable max-len */
-/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-const-assign */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
 
 const covid19ImpactEstimator = (data) => {
-    // input in data
-  
-    // Destructuring data
-  const {
-    timeToElapse,
-    reportedCases
-  } = data;
-  
+  // input in data
 
-    //    calculate InfectionsByRequestedTime
-  const calculateInfectionsByRequestedTime = (currentlyInfected) => {
-      // eslint-disable-next-line radix
-    const factor = parseInt(timeToElapse / 3);
-    return currentlyInfected * (2 ** factor);
-  };
-  
-    // best case estimation
+  // Destructuring data
+  const { periodType, timeToElapse, reportedCases } = data;
+
   const impact = {};
-  
-    // challenge 1
-  impact.currentlyInfected = reportedCases * 10;
-  impact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(impact.currentlyInfected);
-  //challenge 2
-  impact.servereCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
-  impact.hospitalBedsByRequestedTime = calculateAvailableBeds(impact.severeCasesByRequestedTime);
-  
-  
-    // the severe case estimation
   const severeImpact = {};
-    // challenge 1
+  // Custom functions and variables
+
+  // challenge 1
+  impact.currentlyInfected = reportedCases * 10;
   severeImpact.currentlyInfected = reportedCases * 50;
-  severeImpact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(severeImpact.currentlyInfected);
+
+  // normalize days; check for weeks and months if used
+  let timeFactor;
+
+  switch (periodType.trim().toLowerCase()) {
+    case 'months':
+      timeFactor = Math.trunc((timeToElapse * 30) / 3);
+      break;
+    case 'weeks':
+      timeFactor = Math.trunc((timeToElapse * 7) / 3);
+      break;
+    case 'days':
+      timeFactor = Math.trunc((timeToElapse) / 3);
+      break;
+    default:
+  }
+
+  // infections by time passed
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** timeFactor);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** timeFactor);
+
   // challenge 2
+  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
   severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
+
+  impact.hospitalBedsByRequestedTime = calculateAvailableBeds(impact.severeCasesByRequestedTime);
   severeImpact.hospitalBedsByRequestedTime = calculateAvailableBeds(severeImpact.severeCasesByRequestedTime);
- 
+
+  // calculate AvailableBeds
+  const calculateAvailableBeds = (severeCasesByRequestedTime) => {
+    const bedsAvailable = totalHospitalBeds * 0.35;
+    const shortage = bedsAvailable - severeCasesByRequestedTime;
+    const result = shortage < 0 ? shortage : bedsAvailable;
+    return parseInt(result);
+  }
   return {
     data,
     impact,
     severeImpact
   };
 };
-export default covid19ImpactEstimator;  
+export default covid19ImpactEstimator;
