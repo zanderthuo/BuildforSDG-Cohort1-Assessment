90b444d1aea617db5139d972285face69485b0db
Alexander <zanderthuo@gmail.com>
2020-04-13 09:49:21 +0300

    Challenge 2

--- src/estimator.js
+++ src/estimator.js
@@ -24,6 +24,9 @@ const covid19ImpactEstimator = (data) => {
     // challenge 1
   impact.currentlyInfected = reportedCases * 10;
   impact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(impact.currentlyInfected);
+  //challenge 2
+  impact.servereCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+  impact.hospitalBedsByRequestedTime = calculateAvailableBeds(impact.severeCasesByRequestedTime);
   
   
     // the severe case estimation
@@ -31,7 +34,10 @@ const covid19ImpactEstimator = (data) => {
     // challenge 1
   severeImpact.currentlyInfected = reportedCases * 50;
   severeImpact.infectionsByRequestedTime = calculateInfectionsByRequestedTime(severeImpact.currentlyInfected);
-  
+  // challenge 2
+  severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
+  severeImpact.hospitalBedsByRequestedTime = calculateAvailableBeds(severeImpact.severeCasesByRequestedTime);
+ 
   return {
     data,
     impact,
